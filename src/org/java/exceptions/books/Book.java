5d2ec4d0720ea60b065341392a80f86cd50f018d
danielmegale <danim1@live.it>
2023-11-16 16:57:36 +0100

    add snack

--- src/org/java/exceptions/books/Book.java
+++ src/org/java/exceptions/books/Book.java
@@ -1,5 +1,56 @@
 package org.java.exceptions.books;
 
 public class Book {
-
+	private String titolo;
+	private int numeroPagine;
+	private String autore;
+	private String editore;
+	public Book(String titolo,int numeroPagine,String autore,String editore) throws Exception {
+		setTitolo(titolo);
+		setNumeroPagine(numeroPagine);
+		setAutore(autore);
+		setEditore(editore);
+	}
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) throws Exception {
+		if(titolo == null || titolo.isEmpty())
+			throw new Exception("Titol cannot be null");
+		this.titolo = titolo;
+	}
+	public int getNumeroPagine() {
+		return numeroPagine;
+	}
+	public void setNumeroPagine(int numeroPagine) throws Exception {
+		if(numeroPagine <=0)
+			throw new Exception("Page number cannot 0");
+		this.numeroPagine = numeroPagine;
+	}
+	public String getAutore() {
+		return autore;
+	}
+	public void setAutore(String autore) throws Exception {
+		if(autore== null || autore.isEmpty())
+			throw new Exception("Autor cannot be null");
+		this.autore = autore;
+	}
+	public String getEditore() {
+		return editore;
+	}
+	public void setEditore(String editore) throws Exception {
+		if(editore == null || editore.isEmpty())
+			throw new Exception("Editor cannot be null");
+		this.editore = editore;
+	}
+	
+	@Override
+	public String toString() {
+		return   "Titolo"+ " " + titolo+"\n"
+				+"Pagine:" + " " + numeroPagine + "\n"
+				+"Autore:" + " " + autore + "\n"
+				+"editore:" + " " + editore + "\n"
+				+"-------------------------------"+"\n"
+				;
+	}
 }
--- src/org/java/exceptions/books/Main.java
+++ src/org/java/exceptions/books/Main.java
@@ -1,5 +1,85 @@
 package org.java.exceptions.books;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Quanti libri vuoi aggiungere?");
+		int n=in.nextInt();
+		in.nextLine();
+		Book[] books = new Book[n];
+		
+		int arrayLoop=0;
+			
+		try {
+			while (arrayLoop < n) {
+		
+			System.out.println("Inserisci il titolo del libro: ");
+			String titoloInput=in.nextLine();
+			System.out.println("Inserisci il numero di pagine: ");
+			int pagineInput=in.nextInt();
+			in.nextLine();
+			System.out.println("Inserisci l'autore");
+			String autoreInput=in.nextLine();
+			System.out.println("Inserisci l'editore");
+			String editoreInput=in.nextLine();
+			Book b = new Book(titoloInput,pagineInput,autoreInput,editoreInput);
+			System.out.println(b);
+			books[arrayLoop]=b;
+			arrayLoop++;
+			}
+		} catch (Exception e) {
+			System.out.println("Error:"+" "+e.getMessage());
+		}
+		finally{
+			if(in!=null)
+			in.close();
+		}
+		System.out.println("Libro aggiunto correttamente"+"\n"
+				+ "----------------------------");
+		
+		File mioFile = new File("C:\\Users\\danim\\Desktop\\esercizi-java\\java-exceptions-books\\src\\org\\java\\exceptions\\books\\Books.txt");
+		
+		FileWriter myWriter = null;
+		
+		try{
+			myWriter = new FileWriter(mioFile);
+
+			for(int i=0; i < books.length ;i++) {
+				
+				myWriter.write(books[i].toString()+"\n");
+			}
+			
+			} catch(IOException e){
+				System.out.println("Error reading file:" + e.getMessage());
+			}finally {
+				
+				if(myWriter != null)
+				try {
+					myWriter.close();
+				} catch (IOException e) {}
+			}
+		
+		
+		
+		Scanner reader=null;
+		try {
+			reader=new Scanner(mioFile);
+			while(reader.hasNextLine()) {
+				String data = reader.nextLine();
+				System.out.println(data);
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println("Error reading file:" + e.getMessage());
+		}finally {
+			if(reader!=null)
+				reader.close();			
+		}
+		
+}
 }
